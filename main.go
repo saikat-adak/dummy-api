8bba9531a3dbeee59b375ab48d66034063804af9
Saikat Adak <saikatadak2004@gmail.com>
2023-11-08 15:48:35 -0500

    Downgrading the GO runtime to match the requirement of the hosting environment

--- main.go
+++ main.go
@@ -1,55 +1,58 @@
 package main
 
 import (
-    "fmt"
 	"bytes"
-    "net/http"
-    "time"
-    "github.com/gin-gonic/gin"
-    "github.com/tealeg/xlsx"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tealeg/xlsx"
 )
 
-func main(){
+// go 1.21.4
+func main() {
 	router := gin.Default()
 	router.GET("/download/xlsx", getDummyXlsxFile)
 	router.GET("/json", getDummyJson)
 
-    router.Run("localhost:8080")
+	router.Run("localhost:8080")
 }
 
 func getDummyXlsxFile(c *gin.Context) {
 	file := xlsx.NewFile()
-    for i := 0; i < 10; i++ {
-        sheet, err := file.AddSheet(fmt.Sprintf("sheet-%d", i))
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, err.Error())
-            return
-        }
-        row := sheet.AddRow()
-        cell := row.AddCell()
-        cell.Value = "dummy cell!"
-    }
-    var b bytes.Buffer
-    if err := file.Write(&b); err != nil {
-        c.JSON(http.StatusInternalServerError, err.Error())
-        return
-    }
-    downloadName := time.Now().UTC().Format("data-20060102150405.xlsx")
-    c.Header("Content-Description", "File Transfer")
-    c.Header("Content-Disposition", "attachment; filename="+downloadName)
-    c.Data(http.StatusOK, "application/octet-stream", b.Bytes())
+	for i := 0; i < 10; i++ {
+		sheet, err := file.AddSheet(fmt.Sprintf("sheet-%d", i))
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, err.Error())
+			return
+		}
+		row := sheet.AddRow()
+		cell := row.AddCell()
+		cell.Value = "dummy cell!"
+	}
+	var b bytes.Buffer
+	if err := file.Write(&b); err != nil {
+		c.JSON(http.StatusInternalServerError, err.Error())
+		return
+	}
+	downloadName := time.Now().UTC().Format("data-20060102150405.xlsx")
+	c.Header("Content-Description", "File Transfer")
+	c.Header("Content-Disposition", "attachment; filename="+downloadName)
+	c.Data(http.StatusOK, "application/octet-stream", b.Bytes())
 }
-
 
 func getDummyJson(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, users)
+	c.IndentedJSON(http.StatusOK, users)
 }
+
 type user struct {
-    Id        int     `json:"id"`
-    Username  string  `json:"username"`
+	Id       int    `json:"id"`
+	Username string `json:"username"`
 }
+
 var users = []user{
-    {Id: 546, Username: "John"},
-    {Id: 894, Username: "Mary"},
-    {Id: 326, Username: "Jane"},
+	{Id: 546, Username: "John"},
+	{Id: 894, Username: "Mary"},
+	{Id: 326, Username: "Jane"},
 }
