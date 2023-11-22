e3e17ff1397287aedb7c4b4d69f59d4ce2f7d3c7
Saikat Adak <saikatadak2004@gmail.com>
2023-11-22 11:05:13 -0500

    Adding excelize library to create dummy excel file

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	github.com/360EntSecGroup-Skylar/excelize v1.4.1 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
@@ -23,6 +24,7 @@ require (
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/360EntSecGroup-Skylar/excelize"
 	"github.com/gin-gonic/gin"
 	"github.com/tealeg/xlsx"
 )
@@ -13,9 +14,42 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/download/xlsx", getDummyXlsxFile)
+	router.GET("/download/xlsx2", getDummyXlsxUsingExcelize)
+	router.GET("/download/xlsx3", getDummyXlsxFromByteString)
 	router.GET("/json", getDummyJson)
 
 	router.Run("localhost:8080")
+}
+
+func getDummyXlsxUsingExcelize(c *gin.Context) {
+	xlsx := excelize.NewFile()
+	// Create a new sheet.
+	index := xlsx.NewSheet("Sheet2")
+	// Set value of a cell.
+	xlsx.SetCellValue("Sheet2", "A2", "Hello world.")
+	xlsx.SetCellValue("Sheet1", "B2", 100)
+	// Set active sheet of the workbook.
+	xlsx.SetActiveSheet(index)
+
+	// var b *bytes.Buffer
+	b, err := xlsx.WriteToBuffer()
+	if err != nil {
+		// return nil, err
+	}
+	downloadName := time.Now().UTC().Format("data-20060102150405.xlsx")
+	c.Header("Content-Description", "File Transfer")
+	c.Header("Content-Disposition", "attachment; filename="+downloadName)
+	c.Data(http.StatusOK, "application/octet-stream", b.Bytes())
+}
+
+func getDummyXlsxFromByteString(c *gin.Context) {
+
+	strBytes := []byte("paste your byte string here.")
+
+	downloadName := time.Now().UTC().Format("data-20060102150405.xlsx")
+	c.Header("Content-Description", "File Transfer")
+	c.Header("Content-Disposition", "attachment; filename="+downloadName)
+	c.Data(http.StatusOK, "application/octet-stream", strBytes)
 }
 
 func getDummyXlsxFile(c *gin.Context) {
